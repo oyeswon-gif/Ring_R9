467df31ec6e7c34734a1adfe93fbce6dbb749a2e
oyeswon-gif <oyeswon@gmail.com>
2026-04-21 12:28:02 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAsuMM6ABQG8oGF06gSLj8-2w9NotH6Edg",
+  authDomain: "wedding-ring-app-810cd.firebaseapp.com",
+  projectId: "wedding-ring-app-810cd",
+  storageBucket: "wedding-ring-app-810cd.firebasestorage.app",
+  messagingSenderId: "535240325499",
+  appId: "1:535240325499:web:be270ef820153b7ab1a5de"
+};
